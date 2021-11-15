cadc7c444dbda01a9d3ec1f573be3179b98c1df2
AmineKheldouni <aminekheldouni@gmail.Com>
2021-11-15 19:29:51 +0100

    requester OK

--- src/utils/requester.py
+++ src/utils/requester.py
@@ -1,13 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+import argparse
+import numpy as np
 
 URL_MT = "https://mission-transition-ecologique.beta.gouv.fr/api/temp/aids/"
 URL_AT = "https://aides-territoires.beta.gouv.fr/api/aids/all/"
 URL_AT_THEMES = "https://aides-territoires.beta.gouv.fr/api/themes/"
 
+
 class Requester:
     def __init__(self) -> None:
         pass
+
     def get_aids(self) -> list:
         """
         Get all aids from the API.
@@ -16,7 +20,7 @@ class Requester:
         data = response.json()
         aids = data["results"]
         return aids
-    
+
     def get_aid(self, aid_id: str) -> dict:
         """
         Get an aid from the API.
@@ -24,8 +28,8 @@ class Requester:
         response = requests.get(URL_AT + aid_id)
         data = response.json()
         aid = data["results"]
-        return aid 
-    
+        return aid
+
     def get_theme_aids(self, theme_id: str) -> list:
         """
         Get aids from the API with a theme.
@@ -34,7 +38,7 @@ class Requester:
         data = response.json()
         aids = data["results"]
         return aids
-    
+
     def get_themes(self) -> list:
         """
         Get all themes from the API.
@@ -43,6 +47,7 @@ class Requester:
         data = response.json()
         themes = data["results"]
         return themes
+
     def get_aids_mt(self) -> list:
         """
         Get all aids from the Mission Transition API.
@@ -51,24 +56,22 @@ class Requester:
         data = response.json()
         aids = data
         return aids
-    
+
+
 elt = Requester()
 
-print("### Mission Transition ### ")
-mt_aids = elt.get_aids_mt()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get aids from the API.")
+    # parser.add_argument('--help', metavar='N', type=int, help='', default=None)
+    args = parser.parse_args()
 
-print(len(mt_aids))
-print(mt_aids[0].keys())
+    R = Requester()
 
+    mt_aids = R.get_aids_mt()
 
-print("### Aides Territoires ###")
-l = elt.get_aids()
-t = elt.get_themes()
-print(len(l))
-print(l[0])
-print(l[19].keys())
-print(t[0])
-
-print("####")
-print(len(t))
-print(t[0].keys())
+    print("Colonnes aides mission transition: ", mt_aids[0].keys())
+    l = R.get_aids()
+    print("Colonnes aides aides territoires: ", l[0].keys())
+    t = R.get_themes()
+    print("Nombre de thématiques: ", len(t))
+    print("Nombre de sous thématiques: ", np.sum([len(x["categories"]) for x in t]))
